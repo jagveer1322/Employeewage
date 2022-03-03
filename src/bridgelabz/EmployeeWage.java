abef315ae2284a59646c2fb536e13f0168196ddc
jagveer <jagveersingh.55555@gmail.com>
2022-03-03 17:11:22 +0530

    my project

--- src/bridgelabz/EmployeeWage.java
+++ src/bridgelabz/EmployeeWage.java
@@ -1,5 +1,16 @@
 package bridgelabz;
 
 public class EmployeeWage {
-
+	public static void main(String[] args) {
+		System.out.println("Welcome Employewage");
+		int check = (int)Math.floor(Math.random() * 10)%2;
+        if (check == 1)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+	}
 }
