615f5a43c713639cbea51a198ed7368a7a1afec3
jagveer <jagveersingh.55555@gmail.com>
2022-03-04 08:14:46 +0530

    update of uc2

--- src/bridgelabz/EmployeeWage.java
+++ src/bridgelabz/EmployeeWage.java
@@ -1,12 +1,16 @@
 package bridgelabz;
 import java.util.Random;
 public class EmployeeWage {
+	static final int W_P_R = 20;
 	public static void main(String[] args) {
 		System.out.println("Welcome Employewage");
 		int check = (int)Math.floor(Math.random() * 10)%2;
         if (check == 1)
         {
+        	int emphrs = 8;
+        	int empwage = W_P_R*emphrs;
             System.out.println("Employee is Present");
+            System.out.println("Daily wage is " +empwage);
         }
         else
         {
