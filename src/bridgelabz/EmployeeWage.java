5afd83d479fa7e7d2a45b077feeda1e2c9276795
jagveer <jagveersingh.55555@gmail.com>
2022-03-04 09:18:52 +0530

    update commit uc4

--- src/bridgelabz/EmployeeWage.java
+++ src/bridgelabz/EmployeeWage.java
@@ -10,24 +10,19 @@ public class EmployeeWage {
 		int emphrs = 0;
 		int empwage = 0;
 		int check = (int)Math.floor(Math.random() * 10)%2;
-        if (check == I_P_T)
-        {
-        	System.out.println("Employee is part time");
+		switch (check)
+		{
+        case I_P_T : System.out.println("Employee is present part time");
             emphrs = 4;
-    }
-    else 
-    	if(check == I_F_T)
-    	{
-        System.out.println("Employee is full time");
-        emphrs = 8;
+            break;
+        case I_F_T : System.out.println("Employee is  full time");
+            emphrs = 8;
+            break;
+        default : System.out.println("Employee is Absent");
+            emphrs = 0;
         }
-    else {
-        System.out.println("Employee  Absent");
-        emphrs = 0;
-         }
         empwage = W_P_H*emphrs;
-        System.out.println("Employee wage = " +empwage );
-        	
+        System.out.println("Employee wage =" +empwage );
             
       
 	}
