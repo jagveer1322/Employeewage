2225c4eaced93cffb0a7421ef669e3c502ce6b71
jagveer <jagveersingh.55555@gmail.com>
2022-03-04 09:39:30 +0530

    update commit uc6

--- src/bridgelabz/EmployeeWage.java
+++ src/bridgelabz/EmployeeWage.java
@@ -4,14 +4,17 @@ public class EmployeeWage {
 	static final int W_P_H = 20;
 	static final int I_P_T = 1;
 	static final int I_F_T = 2;
-	
+	static final int M_W_D = 20;
+    static final int M_W_H = 100;
 	public static void main(String[] args) {
 		System.out.println("Welcome Employewage");
 		int emphrs = 0;
 		int empwage = 0;
 		int totalempwage = 0;
         int totalworkingdays = 0;
-        while (totalworkingdays < 20) {
+        int totalworkinghours =0;
+        while (totalworkingdays < M_W_D && totalworkinghours <= M_W_H)
+        {
             int check = (int) Math.floor(Math.random() * 10) % 3;
             switch (check) {
                 case I_P_T:
@@ -25,9 +28,12 @@ public class EmployeeWage {
             }
             empwage = W_P_H * emphrs;
             totalempwage = totalempwage + empwage;
+            totalworkinghours = totalworkinghours + emphrs;
             totalworkingdays++;
         }
-        System.out.println("Employee wage is " + totalempwage);
+        System.out.println("Total Working Hours = " +totalworkinghours);
+        System.out.println("Total Working Days = " +totalworkingdays);
+        System.out.println("Total Employee wage is " +totalempwage);
       
 	}
 }
